db48277ec5730c6e1f27396e616f0b690483681c
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-10-18 14:35:11 +0300

    Start new project

---
+++ src/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(count + " " + "Hello, Andrii");
+            count++;
+        }
+    }
+}
